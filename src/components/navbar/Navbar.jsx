f7e059c54fb124f262ef23d71aaad9d379c36351
vargaae <vargaae@hotmail.com>
2024-01-22 00:50:49 +0100

    003-Creating NavBar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+import { ArrowDropDown, Notifications, Search } from "@mui/icons-material";
+import "./navbar.scss";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.scrollY === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
+  console.log(isScrolled)
+  return (
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+      <div className="container">
+        <div className="left">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/74/TLC_Logo.svg"
+            alt="tlc logo"
+          />
+          <span>REALITÁS</span>
+          <span>Series</span>
+          <span>Movies</span>
+          <span>New and popular</span>
+          <span>My List</span>
+        </div>
+        <div className="right">
+          <Search className="icon" />
+          <span>KID</span>
+          <Notifications className="icon" />
+          <img
+            src="https://freesvg.org/img/abstract-user-flat-4.png"
+            alt="User Profile Image"
+          />
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
